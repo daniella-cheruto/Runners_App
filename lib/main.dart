273d61f4bb9b33bf39aa5f0d4f75d7907d357f3f
daniella-cheruto <daniella.mutai@strathmore.edu>
2025-09-13 23:12:10 +0300

    feat:Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
-// import 'screens/register_screen.dart';
+ import 'screens/register_screen.dart';
 import 'screens/forgot_password_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/profile_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Needed before Firebase
+  await Firebase.initializeApp(); //  Initialize Firebase
   runApp(const MyApp());
 }
 
@@ -16,13 +22,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Runners App',
-      initialRoute: '/login',
+      initialRoute: '/', //start with wrapper
       routes: {
+         '/': (context) => const AuthWrapper(),
         '/login': (context) => const LoginScreen(),
-        // '/register': (context) => const RegisterScreen(),
+         '/register': (context) => const RegisterScreen(),
         '/forgot-password': (context) => const ForgotPasswordScreen(),
         '/home': (context) => const HomeScreen(),
         '/profile': (context) => const ProfileScreen(),
+      },
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        if (snapshot.hasData) {
+          return const HomeScreen(); // user already logged in
+        }
+        return const LoginScreen(); // no user, go to login
       },
     );
   }
